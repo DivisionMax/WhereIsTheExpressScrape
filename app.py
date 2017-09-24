1cf98e689b7dd03094dac899a069a2896c2fafe4
Gavin Wiener <gavin.w@superbalist.com>
2017-09-24 19:32:59 +0200

    Scraping the stop data

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 import csv
 import sys
 import logging
+import time
+from bs4 import BeautifulSoup
 
 filename = 'stops.csv'
 
+url_timetable = 'https://myciti.org.za/en/timetables/route-stop-timetables/'
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
-
-@app.route('/stops', methods=['GET'])
 def stops():
     options = []
     with open(filename, 'r') as csvfile:
@@ -27,9 +26,29 @@ def stops():
             sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
 
     logging.debug(options)
-    return render_template('stops.html', options=options)
+    return render_template('index.html', options=options)
 
 
-@app.route('/stops/timetable', methods=['POST'])
+@app.route('/timetable')
 def timetable():
-    return render_template('timetable.html')
+    stop_id = request.args.get('id')
+    weekday = time.strftime("%A")
+    params = {
+        'timetable[weekday]': weekday,
+        'timetable[station]': stop_id,
+    }
+    response = requests.get(url_timetable, params)
+    html = response.content
+    print(html)
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find('table', attrs={'id': 'bodytable'})
+    trs = table.find_all('tr')
+    rows = []
+    for row in trs:
+        data = []
+        for td in row.find_all('td'):
+            print(td)
+            data.append(td.text)
+        rows.append(data)
+    # Each row is '<time>	<bus>	<direction>'
+    return render_template('timetable.html', rows=rows)
